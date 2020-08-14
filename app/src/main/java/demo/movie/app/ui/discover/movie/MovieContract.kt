a8f3be2cc51fb665538915fe9b302d7d7530ed20
Pavel Mikrasov <mikrasov.p@gmail.com>
2020-08-14 14:57:34 +0300

    movie card was added, base fragments' interfaces methods were added, dto for recycler preview were added

--- app/src/main/java/demo/movie/app/ui/discover/movie/MovieContract.kt
+++ app/src/main/java/demo/movie/app/ui/discover/movie/MovieContract.kt
@@ -1,11 +1,24 @@
 package demo.movie.app.ui.discover.movie
 
+import demo.movie.app.model.dto.MoviePreviewDto
 import demo.movie.app.ui.mvp.MvpPresenter
 import demo.movie.app.ui.mvp.MvpView
 
 interface MovieContract {
 
-    interface MovieView : MvpView
+    interface MovieView : MvpView{
+        fun showPopular(popularMovieList: List<MoviePreviewDto>)
+
+        fun showTrending(trendingMovieList: List<MoviePreviewDto>)
+
+        fun showTopRated(topRatedMovieList: List<MoviePreviewDto>)
+
+        fun showLoadError()
+
+        fun showLoadingProgressBar()
+
+        fun showReloadError()
+    }
 
     interface MoviePresenter : MvpPresenter<MovieView>
 
--- app/src/main/java/demo/movie/app/ui/discover/movie/MovieFragment.kt
+++ app/src/main/java/demo/movie/app/ui/discover/movie/MovieFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import demo.movie.app.R
+import demo.movie.app.model.dto.MoviePreviewDto
 
 class MovieFragment : Fragment(), MovieContract.MovieView {
 
@@ -20,6 +21,30 @@ class MovieFragment : Fragment(), MovieContract.MovieView {
         savedInstanceState: Bundle?
     ): View? {
         return layoutInflater.inflate(R.layout.discover_movies_fragment, container, false)
+    }
+
+    override fun showPopular(popularMovieList: List<MoviePreviewDto>) {
+        TODO("Not yet implemented")
+    }
+
+    override fun showTrending(trendingMovieList: List<MoviePreviewDto>) {
+        TODO("Not yet implemented")
+    }
+
+    override fun showTopRated(topRatedMovieList: List<MoviePreviewDto>) {
+        TODO("Not yet implemented")
+    }
+
+    override fun showLoadError() {
+        TODO("Not yet implemented")
+    }
+
+    override fun showLoadingProgressBar() {
+        TODO("Not yet implemented")
+    }
+
+    override fun showReloadError() {
+        TODO("Not yet implemented")
     }
 
 }
