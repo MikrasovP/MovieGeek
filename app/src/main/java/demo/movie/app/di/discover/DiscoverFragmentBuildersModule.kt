cc33c31ecbd66c93486565c702f88805e3d12630
Pavel Mikrasov <mikrasov.p@gmail.com>
2020-08-16 21:34:55 +0300

    MoviePresenter is injected, fallback icon was fixed

--- app/src/main/java/demo/movie/app/di/discover/DiscoverFragmentBuildersModule.kt
+++ app/src/main/java/demo/movie/app/di/discover/DiscoverFragmentBuildersModule.kt
@@ -1,19 +1,33 @@
 package demo.movie.app.di.discover
 
+import dagger.Binds
+import dagger.BindsInstance
 import dagger.Module
+import dagger.Provides
 import dagger.android.ContributesAndroidInjector
+import demo.movie.app.ui.discover.movie.MovieContract
 import demo.movie.app.ui.discover.movie.MovieFragment
+import demo.movie.app.ui.discover.movie.MoviePresenter
 import demo.movie.app.ui.discover.profile.ProfileFragment
 import demo.movie.app.ui.discover.tv.TvFragment
 
 @Module
 abstract class DiscoverFragmentBuildersModule {
+
+    companion object {
+        @Provides
+        fun providesMoviePresenter() = MoviePresenter()
+    }
+
     @ContributesAndroidInjector
     abstract fun contributeMovieFragment(): MovieFragment
+
+    @Binds
+    abstract fun provideMoviePresenter(moviePresenter: MoviePresenter): MovieContract.MoviePresenter
 
     @ContributesAndroidInjector
     abstract fun contributeTvFragment(): TvFragment
 
     @ContributesAndroidInjector
-    abstract fun contributeProfileFragment() : ProfileFragment
+    abstract fun contributeProfileFragment(): ProfileFragment
 }
