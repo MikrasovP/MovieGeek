58451b121942ee92b3b83fb5ecddf5203ef14ebc
Pavel Mikrasov <mikrasov.p@gmail.com>
2020-08-27 14:07:42 +0300

    AdapterProvider added

--- app/src/main/java/demo/movie/app/ui/discover/recycler/adapters/AdapterProvider.kt
+++ app/src/main/java/demo/movie/app/ui/discover/recycler/adapters/AdapterProvider.kt
@@ -1,4 +1,46 @@
 package demo.movie.app.ui.discover.recycler.adapters
 
-class AdapterProvider {
+import demo.movie.app.util.image.BaseImageLoader
+import javax.inject.Inject
+
+class AdapterProvider @Inject constructor() : BaseAdapterProvider(){
+
+    private var adapterPopularMovies: MovieAdapter? = null
+
+    private var adapterTrendingMovies: MovieAdapter? = null
+
+    private var adapterTopRatedMovies: MovieAdapter? = null
+
+    @Inject
+    lateinit var imageLoader: BaseImageLoader
+
+    override fun getPopularMoviesAdapter(): MovieAdapter {
+        return if (adapterPopularMovies == null){
+            val adapter = MovieAdapter(onMovieClick, imageLoader)
+            adapterPopularMovies = adapter
+            adapter
+        } else {
+            adapterPopularMovies as MovieAdapter
+        }
+    }
+
+    override fun getTrendingMoviesAdapter(): MovieAdapter {
+        return if (adapterTrendingMovies == null){
+            val adapter = MovieAdapter(onMovieClick, imageLoader)
+            adapterTrendingMovies = adapter
+            adapter
+        } else {
+            adapterTrendingMovies as MovieAdapter
+        }
+    }
+
+    override fun getTopRatedMoviesAdapter(): MovieAdapter {
+        return if (adapterTopRatedMovies == null){
+            val adapter = MovieAdapter(onMovieClick, imageLoader)
+            adapterTopRatedMovies = adapter
+            adapter
+        } else {
+            adapterTopRatedMovies as MovieAdapter
+        }
+    }
 }
--- app/src/main/java/demo/movie/app/ui/discover/recycler/adapters/BaseAdapterProvider.kt
+++ app/src/main/java/demo/movie/app/ui/discover/recycler/adapters/BaseAdapterProvider.kt
@@ -1,4 +1,14 @@
 package demo.movie.app.ui.discover.recycler.adapters
 
-interface BaseAdapterProvider {
+import demo.movie.app.model.dto.movie.MoviePreviewDto
+
+abstract class BaseAdapterProvider {
+    var onMovieClick: (MoviePreviewDto) -> Unit = {}
+
+    abstract fun getPopularMoviesAdapter(): MovieAdapter
+
+    abstract fun getTrendingMoviesAdapter(): MovieAdapter
+
+    abstract fun getTopRatedMoviesAdapter(): MovieAdapter
+
 }
