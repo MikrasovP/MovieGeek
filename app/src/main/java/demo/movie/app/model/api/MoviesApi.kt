5071d20415f094605c3da2df6527e563f56d7540
Pavel Mikrasov <mikrasov.p@gmail.com>
2020-09-24 10:13:00 +0300

    movie detail method was added to api

--- app/src/main/java/demo/movie/app/model/api/MoviesApi.kt
+++ app/src/main/java/demo/movie/app/model/api/MoviesApi.kt
@@ -1,8 +1,10 @@
 package demo.movie.app.model.api
 
+import demo.movie.app.model.dto.movie.MovieDetailDto
 import demo.movie.app.model.dto.movie.MoviesResponseResult
 import io.reactivex.rxjava3.core.Observable
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface MoviesApi {
@@ -26,5 +28,10 @@ interface MoviesApi {
     fun getTopRated(
         @Query("api_key") api_key: String
     ): Observable<MoviesResponseResult>
+
+    @GET("movie/{movie_id}")
+    fun getMovie(
+        @Path("movie_id") movieId: Int
+    ): Observable<MovieDetailDto>
 
 }
